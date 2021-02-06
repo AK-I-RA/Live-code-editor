845f79c3c7ca0c257aef2e85cb50477318b01d72
Ankit <ankitrawat7895@gmail.com>
2021-02-06 15:44:04 -0800

    Add files

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ var htmlEditor = ace.edit("html");
 htmlEditor.setTheme("ace/theme/cobalt");
 htmlEditor.session.setMode("ace/mode/html");
 htmlEditor.resize();
+htmlEditor.setHighlightActiveLine(false)
